29611a721ef0264b64f8481252cf6fefc480ec5f
ARSDEEP1 <135114422+ARSDEEP1@users.noreply.github.com>
2023-06-10 16:09:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,23 @@
-const tab = document.querySelector('.tab');
-const itemsParent = document.querySelector('.tab__items');
-const items = document.querySelectorAll('.tab__item');
-const indicator = document.querySelector('.tab__indicator');
 
-TweenMax.set(tab, { transformPerspective: 400, transformOrigin:'center center'});
+        const theBody = document.querySelector('body');
+        const openNav = document.querySelector('.menu-bar button');
+        const closeNav = document.querySelector('.close-nav button');
+        const Navbar = document.querySelector('.navbar');
 
-const getActiveTabIndex = () => {
-  return Array.from(items).findIndex(item => item.classList.contains('tab__item--active'));
-}
+        // function bodyScroll(){
+        //     if(Navbar.classList.contains('show')){
+        //         theBody.classList.add('hide-scroll');
+        //     }
+        //     else if(theBody.classList.contains('hide-scroll')){
+        //         theBody.classList.remove('hide-scroll');
+        //     }
+        // }
 
-const getIndicatorPos = () => {
-  const { x: tabX } = tab.getBoundingClientRect();
-  const { width: itemWidth, x: itemX } = items[getActiveTabIndex()].getBoundingClientRect();
-  const { width: indicatorWidth } = indicator.getBoundingClientRect();
- 
-  const indicatorPosition = (itemX + itemWidth/2 - indicatorWidth/2) - tabX;
-  
-  return indicatorPosition;
-}
+        function showHide(){
+            Navbar.classList.toggle('show');
+            // bodyScroll();
+        }
 
-const setIndicatorPos = () => {
-  TweenMax.set(indicator, { x: getIndicatorPos()});  
-}
+        openNav.onclick = showHide;
+        closeNav.onclick = showHide;
 
-const tilt = (e) => {
-  if (e.target.classList.contains('tab__item')) {
-    const pageX = e.pageX ? e.pageX : e.touches[0].pageX;
-    const rotate = pageX - window.innerWidth/2; 
-  
-    TweenMax.to(e.target, .2, { scale: .8, ease: Power4.easeOut });
-    TweenMax.to(tab, .5, { rotationY: 0.03 * rotate, rotationX: -Math.abs(0.015 * rotate), rotationZ: '-0.1' });
-  }
-}
-
-const activateTab = (e) => {
-  if (e.target.classList.contains('tab__item')) {
-    items.forEach(item => item.classList.remove('tab__item--active'));
-    e.target.classList.add('tab__item--active');
-    
-    TweenMax.to(indicator, .5, { x: getIndicatorPos() });
-    TweenMax.to(e.target, .3, { scale: 1, ease: Back.easeOut.config(5) });
-    TweenMax.to(tab, .5, { rotationY: 0, rotationX: 0, rotationZ: 0 }); 
-  }
-}
-
-setIndicatorPos();
-
-itemsParent.addEventListener('touchstart', tilt);
-itemsParent.addEventListener('mousedown', tilt);
-
-itemsParent.addEventListener('touchend', activateTab);
-itemsParent.addEventListener('mouseup', activateTab);
-
-window.addEventListener('resize', setIndicatorPos);
